409a754aac194d460fa0499e7d345f0f9113ca26
Zach <Zacharydehooge@gmail.com>
2025-05-30 22:51:15 -0400

    Add API wrapper submodule and update script

--- go.mod
+++ go.mod
@@ -2,10 +2,18 @@ module github.com/Zachdehooge/space-traders
 
 go 1.24.0
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/Zachdehooge/space_trader_go v0.0.0-20250531013831-f3264188a9d3
+	github.com/fatih/color v1.18.0
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/HOWZ1T/space_trader v0.1.3 // indirect
-	github.com/Zachdehooge/space_trader_go v0.0.0-20250530194323-b09c0c512ae0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 )
+
+replace github.com/Zachdehooge/space_trader_go => ./api-wrapper-go
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	spacetrader "github.com/Zachdehooge/space_trader_go"
+	"github.com/fatih/color"
+	"github.com/joho/godotenv"
 	"log"
-	"net/http"
+	"os"
 )
-
-type StatusResponse struct {
-	Status string `json:"status"`
-}
 
 func check(err error) {
 	if err != nil {
@@ -18,28 +15,25 @@ func check(err error) {
 }
 func main() {
 
-	/*	err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-
-		token := os.Getenv("TOKEN")
-
-		spaceTrader := space_trader.New(token, "FLASH")
-		status, err := spaceTrader.ApiStatus()
-		check(err)
-		fmt.Printf("API Status: %s", status)*/
-
-	resp, err := http.Get("https://api.spacetraders.io/")
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Request failed: %v", err)
-	}
-	defer resp.Body.Close()
-
-	var data StatusResponse
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		log.Fatalf("JSON decode failed: %v", err)
+		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println("Status:", data.Status)
+	token := os.Getenv("TOKEN")
+
+	spaceTrader := spacetrader.New(token, "FLASH")
+	status, err := spaceTrader.ApiStatus()
+	resetDate, err := spaceTrader.LastDate()
+	nextReset, err := spaceTrader.NextReset()
+	resetFreq, err := spaceTrader.ResetFreq()
+
+	check(err)
+
+	d := color.New(color.FgCyan, color.Bold)
+
+	_, _ = d.Printf("API Status: %s\n", status)
+	_, _ = d.Printf("Last Reset Date: %s\n", resetDate)
+	_, _ = d.Printf("Next Reset Date: %s\n", nextReset)
+	_, _ = d.Printf("Reset Frequency: %s\n", resetFreq)
 }
